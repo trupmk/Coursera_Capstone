2a077c46e946893099d5a1345d1f0bfd8c2e0635
trupmk <57889272+trupmk@users.noreply.github.com>
2019-11-18 13:20:29 +0530

    Update Capstone Basic_Project.py

--- Capstone Basic_Project.py
+++ Capstone Basic_Project.py
@@ -1,18 +1,5 @@
-#!/usr/bin/env python
-# coding: utf-8
+This notebook is mainly used for the Capstone project 
+In[2]: import pandas as pd
+In[4]: import numpy as np
 
-# This notebook is mainly used for the Capstone project 
-
-# In[2]:
-
-
-import pandas as pd
-
-
-# In[4]:
-
-
-import numpy as np
-
-
-# Hello Capstone Project Course
+Hello Capstone Project Course
