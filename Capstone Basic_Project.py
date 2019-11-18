12ffa3a7cd87488133cf9ef6f70eb98941b7d08c
trupmk <57889272+trupmk@users.noreply.github.com>
2019-11-18 13:26:51 +0530

    Add files via upload

--- Capstone Basic_Project.py
+++ Capstone Basic_Project.py
@@ -1,5 +1,18 @@
-This notebook is mainly used for the Capstone project 
-In[2]: import pandas as pd
-In[4]: import numpy as np
+#!/usr/bin/env python
+# coding: utf-8
 
-Hello Capstone Project Course
+# This notebook is mainly used for the Capstone project 
+
+# In[2]:
+
+
+import pandas as pd
+
+
+# In[4]:
+
+
+import numpy as np
+
+
+# Hello Capstone Project Course
